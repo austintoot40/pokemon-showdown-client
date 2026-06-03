6e3e5b8a484c17b71645492ac5dd5f6bf71b249c
Austin Toot <austintoot@gmail.com>
2026-06-03 11:42:44 -0600

    browser title fix

--- play.pokemonshowdown.com/src/panels.tsx
+++ play.pokemonshowdown.com/src/panels.tsx
@@ -99,7 +99,7 @@ export class PSRouter {
 		const panelState = (PS.leftPanelWidth && room === PS.panel ?
 			PS.leftPanel.id + '..' + PS.rightPanel!.id :
 			room.id);
-		const newTitle = roomid === '' ? 'Showdown!' : `${room.title} - Showdown!`;
+		const newTitle = roomid === '' ? 'Nuzlocke Simulator' : `${room.title} - Nuzlocke Simulator`;
 		let changed: boolean | null = (roomid !== this.roomid);
 
 		this.roomid = roomid;
